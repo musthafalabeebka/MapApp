6ec06d316cda95305753771b492842d6af81f561
Musthafa Labeeb K A <musthafalabeebka@gmail.com>
2025-09-30 11:09:01 +0530

    App commit-3

---
+++ Test2/Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by Musthafa Labeeb K A on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
