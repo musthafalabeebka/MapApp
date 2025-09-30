aedb2ad16457b77a02f1c21019cc45dfc102fcd2
Musthafa Labeeb K A <musthafalabeebka@gmail.com>
2025-09-30 11:36:27 +0530

    map intergration

--- Test2/Test2/ContentView.swift
+++ Test2/Test2/ContentView.swift
@@ -6,14 +6,24 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var position: MapCameraPosition = .region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 10.0159, longitude: 76.3419),
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+    )
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Map(position: $position) {
+                Marker("Kakkanad", coordinate: CLLocationCoordinate2D(latitude: 10.0159, longitude: 76.3419))
+            }
+            .frame(height: 300)
+            .cornerRadius(12)
+            .shadow(radius: 5)
         }
         .padding()
     }
